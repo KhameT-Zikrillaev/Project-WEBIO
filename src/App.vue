ec7826fc7ea387daeacd78150fb416aac58d66d4
KhameT-Zikrillaev <zxamidulla1995@mail.ru>
2024-07-06 02:27:44 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "./components/ContentFIles/Header.vue";
+import Main from "./components/ContentFIles/Main.vue";
+import Footer from "./components/ContentFIles/Footer.vue";
+</script>
+
+<template>
+  <div class="wrapper">
+    <Header/>
+    <Main/>
+    <Footer/>
+  </div>
+</template>
